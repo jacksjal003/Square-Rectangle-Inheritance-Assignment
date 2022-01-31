11439dc523c734d58be8f944428d825fbca1dd61
narmstro2020 <narmstro@warren.k12.in.us>
2022-01-31 12:01:20 -0500

    Updated

--- src/org/WalkerCC_CompSci/Square.java
+++ src/org/WalkerCC_CompSci/Square.java
@@ -3,15 +3,17 @@ package org.WalkerCC_CompSci;
 public final class Square extends Rectangle{
 
     public Square(double sideLength) {
+        super(sideLength, sideLength);  //What goes here?
 
     }
 
     public double getSideLength(){
-
+        return this.length;
     }
 
-    public void setSideLength(double width){
-        
+    public void setSideLength(double sideLength){
+        //What goes here?
+       
     }
 
 
